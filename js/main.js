6263666b79967a9b71fc814159ca469eebd1c320
James Essex <JMEssex@gmail.com>
2017-02-13 11:27:35 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+////////// GLOBAL VARIABLES //////////
+
+var turn = 1
+
+var player1 = {
+  token: 'black',
+}
+
+var player2 = {
+  token: 'red',
+}
+
+function Column(){
+  this.dropCount = 0;
+  this.boardArr = [null,null,null,null,null,null];
+};
+
+var colA = new Column();
+var colB = new Column();
+var colC = new Column();
+var colD = new Column();
+var colE = new Column();
+var colF = new Column();
+var colG = new Column();
+
+
+gameBoard = [colA.boardArr, colB.boardArr, colC.boardArr, colD.boardArr, colE.boardArr, colF.boardArr, colG.boardArr]
+
+////////// HAPPENS IMMEDIATELY //////////
+
+////////// FUNCTIONS //////////
+
+//  Standard check to see who's turn it is.
+function takeTurns() {
+  return (turn % 2 ? 'black' : 'red')
+}
+
+//  Removes the null values from the array before pushing
+function removeNulls(arr){
+  while (arr.indexOf(null) !== -1){
+      var nullIndex = arr.indexOf(null)
+      arr.splice(nullIndex, 1)
+    }
+  console.log(arr)
+}
+
+//  Adds the missing null values back to the array for gameBoard consistency.
+function addNulls(arr){
+  while (arr.length < 6) {
+    arr.push(null)
+  }
+  console.log(arr)
+}
+
+//  Current drop token function.
+function drop(arr, playerToken) {
+  removeNulls(arr)
+  arr.push(playerToken)
+  console.log(arr)
+  addNulls(arr)
+}
+
+//  Check to see if the row is full before droping token.
+function fullCheck(arr, playerToken, columnObj){
+  if (arr.indexOf(null) !== -1 && arr.length <= 6 && columnObj.dropCount <= 6) {
+    drop(arr, playerToken)
+    console.log(arr)
+    columnObj.dropCount++
+    console.log(columnObj.dropCount)
+    turn++
+  } else {
+    alert("This row is already full")
+  }
+}
+
+function clickRow(arr, playerToken, columnObj, fn) {
+//  adding event listener to the .dropper class
+$('.available').on('click', fn())
